58203a3d40b5c4ed09f9bae4b1fea08b3d72b935
Pablo Orduña <pablo.orduna@deusto.es>
2011-02-28 11:55:38 +0100

    [otsoDroid]  * Adding RestCommunications

---
+++ otsoFullJavaCommons/src/otsopack/full/java/network/communication/RestCommunication.java
@@ -0,0 +1,119 @@
+package otsopack.full.java.network.communication;
+
+import otsopack.commons.data.IGraph;
+import otsopack.commons.data.ITemplate;
+import otsopack.commons.exceptions.SpaceNotExistsException;
+import otsopack.commons.exceptions.TSException;
+import otsopack.commons.network.ICommunication;
+import otsopack.commons.network.communication.demand.local.ISuggestionCallback;
+import otsopack.commons.network.communication.event.listener.INotificationListener;
+
+public class RestCommunication implements ICommunication {
+
+	public RestCommunication() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startup() throws TSException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void shutdown() throws TSException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public IGraph read(String spaceURI, String graphURI, long timeout)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGraph read(String spaceURI, ITemplate template, long timeout)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGraph take(String spaceURI, String graphURI, long timeout)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGraph take(String spaceURI, ITemplate template, long timeout)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IGraph query(String spaceURI, ITemplate template, long timeout)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String subscribe(String spaceURI, ITemplate template,
+			INotificationListener listener) throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void unsubscribe(String spaceURI, String subscriptionURI)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String advertise(String spaceURI, ITemplate template)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void unadvertise(String spaceURI, String advertisementURI)
+			throws SpaceNotExistsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void demand(String spaceURI, ITemplate template, long leaseTime,
+			ISuggestionCallback callback) throws TSException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void suggest(String spaceURI, IGraph graph) throws TSException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean callbackIfIHaveResponsabilityOverThisKnowlege(
+			String spaceURI, IGraph triples) throws TSException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean hasAnyPeerResponsabilityOverThisKnowlege(String spaceURI,
+			IGraph triples) throws TSException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
