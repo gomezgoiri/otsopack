bc40ed2398011f80aa50d443ce28022c760fbeeb
Pablo Orduña <pablo.orduna@deusto.es>
2011-03-02 12:15:36 +0100

    [otsoPack]  * Moving the RestServerTest to other class name

---
+++ otsoFullJavaCommons/test/otsopack/full/java/network/communication/AbstractRestServerTesting.java
@@ -0,0 +1,19 @@
+package otsopack.full.java.network.communication;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class AbstractRestServerTesting {
+	protected RestServer rs;
+	
+	@Before
+	public void setUp() throws Exception {
+		this.rs = new RestServer();
+		this.rs.startup();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		this.rs.shutdown();
+	}
+}
