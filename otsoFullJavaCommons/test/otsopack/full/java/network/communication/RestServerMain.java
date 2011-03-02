9fb7bacb040d71c1a93cd0d64794eb2f6c85801f
Pablo Orduña <pablo.orduna@deusto.es>
2011-03-02 15:37:32 +0100

    [otsoPack]  * Moving RestServerMain to the test directory

--- otsoFullJavaCommons/test/otsopack/full/java/network/communication/RestServerMain.java
+++ otsoFullJavaCommons/test/otsopack/full/java/network/communication/RestServerMain.java
@@ -1,5 +1,10 @@
 package otsopack.full.java.network.communication;
 
+/**
+ * 
+ * This class is not a JUnit test, but only to manually test the server.
+ * 
+ */
 public class RestServerMain {
 	public static void main(String [] args) throws Exception {
 		new RestServer().startup();
