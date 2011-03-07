988e61b74e62a4deff7dffb82968dc413ab628ae
Aitor Gómez-Goiri <aitor@gomezgoiri.net>
2011-03-07 18:20:33 +0100

    Query html representation drafted

--- otsoFullJavaCommons/src/otsopack/full/java/network/communication/resources/query/QueryResource.java
+++ otsoFullJavaCommons/src/otsopack/full/java/network/communication/resources/query/QueryResource.java
@@ -14,9 +14,16 @@
 
 package otsopack.full.java.network.communication.resources.query;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.restlet.data.Status;
+import org.restlet.resource.ResourceException;
+
+import otsopack.commons.IController;
+import otsopack.commons.exceptions.SpaceNotExistsException;
 import otsopack.full.java.network.communication.resources.AbstractServerResource;
 import otsopack.full.java.network.communication.resources.spaces.SpaceResource;
 import otsopack.full.java.network.communication.util.HTMLEncoder;
@@ -35,10 +42,17 @@ public class QueryResource extends AbstractServerResource implements IQueryResou
 	
 	@Override
 	public String toHtml() {
+		final StringBuilder bodyHtml = new StringBuilder("<br />\n");
+		bodyHtml.append("\t<fieldset>\n\t<legend>Triples</legend>\n");
+		bodyHtml.append("\t\t<textarea rows=\"10\" cols=\"50\">");
+		bodyHtml.append("triple1, triple2,...");
+		bodyHtml.append("\t\t</textarea>\n");
+		bodyHtml.append("\t</fieldset>\n");
+		
 		return HTMLEncoder.encodeURIs(
-				super.getArguments(ROOT).entrySet(),
-				getRoots().keySet()
-			);
+					super.getArguments(ROOT).entrySet(),
+					null,
+					bodyHtml.toString()); // TODO print NTriples
 	}
 
 	@Override
