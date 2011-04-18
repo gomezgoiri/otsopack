8db6fc376f872cc8c491f5858bb41a38176cdae4
Pablo Orduña <pablo.orduna@deusto.es>
2011-04-18 18:05:26 +0200

    adding RestServer

---
+++ otsoFullJavaCommons/src/otsopack/full/java/network/coordination/discovery/http/server/RestServer.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2008-2011 University of Deusto
+ * 
+ * All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.
+ * 
+ * This software consists of contributions made by many individuals, 
+ * listed below:
+ *
+ * Author: Pablo Orduña <pablo.orduna@deusto.es>
+ */
+package otsopack.full.java.network.coordination.discovery.http.server;
+
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
+public class RestServer {
+	public static final int DEFAULT_PORT = 8185;
+	
+	private final int port;
+	private final Component component;
+	private final OtsopackHttpDiscoveryApplication application;
+	
+	public RestServer(int port, IDiscoveryController controller) {
+		this.port = port;
+		
+	    this.component = new Component();
+	    this.component.getServers().add(Protocol.HTTP, this.port);
+	    
+	    this.application = new OtsopackHttpDiscoveryApplication();
+	    this.application.setController(controller);
+	    this.component.getDefaultHost().attach(this.application);
+	}
+	
+	public RestServer(IDiscoveryController controller){
+		this(DEFAULT_PORT, controller);
+	}
+	
+	public RestServer(int port){
+		this(port, null);
+	}
+	
+	public RestServer(){
+		this(DEFAULT_PORT, null);
+	}
+	
+	public OtsopackHttpDiscoveryApplication getApplication(){
+		return this.application;
+	}
+	
+	public void startup() throws Exception {
+		this.component.start();
+	}
+	
+	public void shutdown() throws Exception {
+		this.component.stop();
+	}
+}
