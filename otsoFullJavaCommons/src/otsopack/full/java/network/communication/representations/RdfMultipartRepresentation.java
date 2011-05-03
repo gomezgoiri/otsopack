0e8b3f0d1375255e8a27713b16778e0e8da6ee0d
Pablo Orduña <pablo.orduna@deusto.es>
2011-05-03 19:03:52 +0200

    Serializing and deserializing RdfMultipartRepresentation

--- otsoFullJavaCommons/src/otsopack/full/java/network/communication/representations/RdfMultipartRepresentation.java
+++ otsoFullJavaCommons/src/otsopack/full/java/network/communication/representations/RdfMultipartRepresentation.java
@@ -15,31 +15,52 @@
 package otsopack.full.java.network.communication.representations;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Vector;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.restlet.data.MediaType;
 import org.restlet.representation.Representation;
 
+import otsopack.commons.authz.entities.User;
 import otsopack.commons.data.Graph;
+import otsopack.commons.data.SemanticFormat;
 import otsopack.commons.data.SignedGraph;
 
 public class RdfMultipartRepresentation extends SemanticFormatRepresentation {
+
+	private static final String CONTENT_TYPE  = "Content-type";
+	private static final String OTSOPACK_USER = "X-OTSOPACK-User";
+	private static final String PAYLOAD       = "payload";
 
 	public RdfMultipartRepresentation(String data) {
 		super(OtsopackConverter.ACROSS_MULTIPART_MEDIA_TYPE, data);
 	}
 
-	public RdfMultipartRepresentation(Map<String, Graph> graphs) {
+	public RdfMultipartRepresentation(Graph [] graphs) {
 		super(OtsopackConverter.ACROSS_MULTIPART_MEDIA_TYPE, convertData(graphs));
 	}
 	
-	private static String convertData(Map<String, Graph> graphs){
-		// TODO
-		return null;
+	private static String convertData(Graph [] graphs){
+		final JSONArray arr = new JSONArray();
+		try{
+			for(Graph graph : graphs){
+				final JSONObject object = new JSONObject();
+				final String contentType = SemanticFormatRepresentationRegistry.getMediaType(graph.getFormat()).getName();
+				object.put(CONTENT_TYPE, contentType);
+				object.put(PAYLOAD, graph.getData());
+				if(graph.getEntity() instanceof User){
+					final User user = (User)graph.getEntity();
+					object.put(OTSOPACK_USER, user.getId());
+				} //TODO: else?
+			}
+		}catch(JSONException e){
+			// TODO: make tests of this part!
+			// TODO: should use a checked exception
+			throw new IllegalStateException("Could not generate JSON data: " + e.getMessage(), e);
+		}
+		
+		return arr.toString();
 	}
 
 	public RdfMultipartRepresentation(Representation representation) throws IOException {
@@ -47,15 +68,14 @@ public class RdfMultipartRepresentation extends SemanticFormatRepresentation {
 	}
 	
 	@Override
-	public SignedGraph [] getGraphs(){
-		
+	public Graph [] getGraphs(){
 		try {
 			final JSONArray arr = new JSONArray(getData());
 			
-			final SignedGraph [] graphs = new SignedGraph[arr.length()];
+			final Graph [] graphs = new Graph[arr.length()];
 			for(int i = 0; i < arr.length(); ++i){
 				final JSONObject obj = arr.getJSONObject(i);
-				graphs[i] = getSignedGraph(obj);
+				graphs[i] = parseGraph(obj);
 			}
 			
 			return graphs;
@@ -65,9 +85,23 @@ public class RdfMultipartRepresentation extends SemanticFormatRepresentation {
 		}
 	}
 	
-	private SignedGraph getSignedGraph(JSONObject obj) throws JSONException {
-		final String contentType = obj.getString("Content-type");
-		obj.getString("payload");
-		return null;
+	private Graph parseGraph(JSONObject obj) throws JSONException {
+		final String contentType    = obj.getString(CONTENT_TYPE);
+		final String data           = obj.getString(PAYLOAD);
+		
+		// Optional fields, required for SignedGraphs
+		final String userId         = obj.optString(OTSOPACK_USER);
+		// TODO: implement a signature for user ID and check it. 
+		// When fixed say it here: http://code.google.com/p/otsopack/issues/detail?id=4
+		
+		final MediaType mediaType = MediaType.valueOf(contentType);
+		if(mediaType == null)
+			throw new JSONException("Unregistered invalid content type in Restlet's MediaType registry: " + contentType);
+		
+		final SemanticFormat format = SemanticFormatRepresentationRegistry.getSemanticFormat(mediaType); 
+		if(userId == null) // Not signed
+			return new Graph(data, format);
+		
+		return new SignedGraph(data, format, new User(userId));
 	}
 }
