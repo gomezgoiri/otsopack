d44adf7ccf236aefcb9470e5ded756d3ba8eaf5d
Pablo Orduña <pablo.orduna@deusto.es>
2011-05-16 09:55:48 +0200

    Adding HttpKernel

---
+++ otsoDroid/otsoDroid/src/otsopack/droid/kernel/HttpKernel.java
@@ -0,0 +1,43 @@
+package otsopack.droid.kernel;
+/*
+ * Copyright (C) 2008-2011 University of Deusto
+ * 
+ * All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.
+ * 
+ * This software consists of contributions made by many individuals, 
+ * listed below:
+ *
+ * Author: FILLME
+ *
+ */
+
+
+import otsopack.commons.authz.entities.IEntity;
+import otsopack.commons.kernel.AbstractKernel;
+import otsopack.full.java.network.RestNetwork;
+import otsopack.full.java.network.coordination.IRegistry;
+
+public class HttpKernel extends AbstractKernel {
+
+	private final int port;
+	private final IEntity signer;
+	private final IRegistry registry;
+	
+	public HttpKernel(int port, IEntity signer, IRegistry registry){
+		this.port     = port;
+		this.signer   = signer;
+		this.registry = registry;
+	}
+	
+	protected void buildKernel(){
+		super.buildKernel();
+		
+		if (this.networkService == null) {
+			this.setNetworkService(new RestNetwork(getController(), port, signer, registry));
+		}
+	}
+
+}
