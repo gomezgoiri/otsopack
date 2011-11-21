8b5b4c6a8e039aa9022b819bc2789b9b8d9b96d3
Pablo Orduña <pablo.orduna@deusto.es>
2011-11-21 12:45:00 +0100

    Ooops [wrong paste]

--- otsoRestletCommons/src/otsopack/restlet/commons/OtsoRestletUtils.java
+++ otsoRestletCommons/src/otsopack/restlet/commons/OtsoRestletUtils.java
@@ -15,23 +15,6 @@ package otsopack.restlet.commons;
 
 import org.restlet.Context;
 
-/*
- * Copyright (C) 2008-2011 University of Deusto
- * 
- * All rights reserved.
- *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution.
- * 
- * This software consists of contributions made by many individuals, 
- * listed below:
- *
- * Author: Pablo Orduña <pablo.orduna@deusto.es>
- */
-package otsopack.restlet.commons;
-
-import org.restlet.Context;
-
 public class OtsoRestletUtils {
 
     public static int lowThreads                = 15;
