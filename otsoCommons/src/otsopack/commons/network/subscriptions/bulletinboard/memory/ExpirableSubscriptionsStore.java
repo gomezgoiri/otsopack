dc3d606b75fd4ae0cc937ae21b984def11c0a1dd
Aitor Gómez-Goiri <aitor@gomezgoiri.net>
2012-06-06 19:29:56 +0200

    wait is interrupted when an update is received also (fixed)

--- otsoCommons/src/otsopack/commons/network/subscriptions/bulletinboard/memory/ExpirableSubscriptionsStore.java
+++ otsoCommons/src/otsopack/commons/network/subscriptions/bulletinboard/memory/ExpirableSubscriptionsStore.java
@@ -39,7 +39,7 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 	protected SortedSet<ExpirableSubscriptions> expirableElements
 					= new ConcurrentSkipListSet<ExpirableSubscriptions>();
 	//guards expirableElement
-	private Lock lock = new ReentrantLock();
+	private Lock expirableElementsLock = new ReentrantLock();
 	private final Object lockElementAdded = new Object();
 	
 	public void stop() {
@@ -50,17 +50,17 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 		final ExpirableSubscriptions expirable = new ExpirableSubscriptions(subs);
 		this.subscriptions.put(subs.getID(), expirable);
 		
-		this.lock.lock();
+		this.expirableElementsLock.lock();
 		try {
 			this.expirableElements.add(expirable);
-	     } finally {
-	         this.lock.unlock();
-	     }
+	    } finally {
+	    	this.expirableElementsLock.unlock();
+	    }
 		
 	    // Just in case the added advertisement is the first one
-		synchronized(this.lockElementAdded) {
-			this.lockElementAdded.notifyAll();
-		}
+	    synchronized(this.lockElementAdded) {
+	    	this.lockElementAdded.notifyAll();
+	    }
 		return subs.getID();
 	}
 
@@ -72,7 +72,7 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 	public Subscription updateSubscription(Subscription changed) {
 		final ExpirableSubscriptions updated = merge( this.subscriptions.get(changed.getID()), changed);
 				
-		this.lock.lock();
+		this.expirableElementsLock.lock();
 		this.subscriptions.putIfAbsent(changed.getID(), updated); // just in case during merge it has expired
 		try {
 			// TODO check whether sortedSet already takes into account the changes in the object.
@@ -80,16 +80,27 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 			this.expirableElements.remove(updated);
 			this.expirableElements.add(updated);
 			//TODO could it be better just calling to Collections.sort()?
-	     } finally {
-	         this.lock.unlock();
-	     }
+	    } finally {
+	        this.expirableElementsLock.unlock();
+	    }
 	     
-	     return updated.getSubscription();
+	    // Just in case the added advertisement is the first one
+		synchronized(this.lockElementAdded) {
+			this.lockElementAdded.notifyAll();
+		}
+	    return updated.getSubscription();
 	}
 	
+	/**
+	 * @param toBeUpdated
+	 * 		Old subscription.
+	 * @param updated
+	 * 		New subscription to be merged with the old one.
+	 * @return
+	 * 		toBeUpdated updated or a new ExpirableSubscriptions if toBeUpdated was null.
+	 */
 	private ExpirableSubscriptions merge(ExpirableSubscriptions toBeUpdated, Subscription updated) {
 		if (toBeUpdated==null) {
-			System.out.println("From scratch.");
 			return new ExpirableSubscriptions(updated);
 		}
 		
@@ -101,11 +112,11 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 		final ExpirableSubscriptions subs = this.subscriptions.remove(subscriptionId);
 		
 		if( subs!=null ) {
-			this.lock.lock();
+			this.expirableElementsLock.lock();
 			try {
 				this.expirableElements.remove(subs);
 		     } finally {
-		         this.lock.unlock();
+		         this.expirableElementsLock.unlock();
 		     }
 		     return subs.getSubscription();
 		}
@@ -126,18 +137,21 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 		long remainingTime;
 		
 		while( !this.cancel ) {
-			if( this.expirableElements.isEmpty() ) {
+			this.expirableElementsLock.lock();
+			if( this.expirableElements.isEmpty() ) { // locked just in case it is just changing
+				this.expirableElementsLock.unlock();
 				synchronized(this.lockElementAdded) {
 					try {
 						// wait until a new notificable element is added
+						//System.out.println("Waiting ad-infinitum.");
 						this.lockElementAdded.wait();
+						//System.out.println("Finished!");
 					} catch (InterruptedException e) {}
 				}
 			} else {
-				this.lock.lock();
 				try {
 					final ExpirableSubscriptions element = this.expirableElements.first();
-					remainingTime = element.expiration - System.currentTimeMillis();
+					remainingTime = element.getRemainingTime();
 					if( remainingTime<=0 ) {
 						this.expirableElements.remove(element);
 						
@@ -145,13 +159,14 @@ public class ExpirableSubscriptionsStore implements ISubscriptionStore, Runnable
 						this.subscriptions.remove(element.getSubscription().getID());
 					}
 			     } finally {
-			         this.lock.unlock();
+			         this.expirableElementsLock.unlock();
 			     }
 			     // if another element has inserted in the first position between
 			     // the assignment and this if we are going to wait unnecessarily
 			     if( remainingTime>0 ) {
 					synchronized(this.lockElementAdded) {
 						try {
+							//System.out.println("Waiting "+remainingTime+"s");
 							this.lockElementAdded.wait(remainingTime);
 						} catch (InterruptedException e) {
 							// it's ok if another Thread interrupts it,
@@ -188,13 +203,17 @@ class ExpirableSubscriptions implements Comparable<ExpirableSubscriptions> {
 	}
 	
 	public Subscription getSubscription() {
-		this.subscription.setLifetime(expiration - System.currentTimeMillis());
+		this.subscription.setLifetime(getRemainingTime());
 		return this.subscription;
 	}
 	
 	public void setExtraTime(long extratime) {
 		this.subscription.setLifetime(extratime);
 		refreshExpiration();
+	}
+	
+	public long getRemainingTime() {
+		return this.expiration - System.currentTimeMillis();
 	}
 	
 	private void refreshExpiration() {
