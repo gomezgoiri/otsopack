cf337b6f4a7454cd25e44651a23f6267d840188e
Aitor Gómez Goiri <aitor.gomez@deusto.es>
2013-11-20 21:02:55 +0100

    Copying class with sample triples from otsoCommons to otsoSE. Since it was in the test source folder of otsoCommons, the otsoSE module couldn't reuse it.

---
+++ otsoSE/src/test/java/otsopack/commons/sampledata/Example.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2008 onwards University of Deusto
+ * 
+ * All rights reserved.
+ *
+ * This software is licensed as described in the file COPYING, which
+ * you should have received as part of this distribution.
+ * 
+ * This software consists of contributions made by many individuals, 
+ * listed below:
+ *
+ * Author: Aitor Gómez Goiri <aitor.gomez@deusto.es>
+ */
+
+package otsopack.commons.sampledata;
+
+public class Example {
+	static final public String subj1 = "http://subject1";
+	static final public String subj2 = "http://subject2";
+	static final public String subj3 = "http://subject3";
+	static final public String subj4 = "http://subject4";
+	static final public String subj5 = "http://subject5";
+	static final public String subj6 = "http://subject6";
+	static final public String prop1 = "http://predicate1";
+	static final public String prop2 = "http://predicate2";
+	static final public String prop3 = "http://predicate3";
+	static final public String prop4 = "http://predicate4";
+	static final public String prop5 = "http://predicate5";
+	static final public String prop6 = "http://predicate6";
+	static final public String obj1 = "http://object1";
+	static final public String obj2 = "http://object2";
+	static final public String obj3 = "http://object3";
+	static final public String obj4 = "http://object4";
+	static final public String obj5 = "http://object5";
+	static final public String obj6 = "http://object6";
+	static final public Integer obj7 = new Integer(21);
+	static final public Boolean obj8 = new Boolean(true);
+	static final public Integer obj9 = new Integer(27);
+	static final public Double obj10 = new Double(28.456);
+}
